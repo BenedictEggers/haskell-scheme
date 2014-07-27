7f088db1fef07513f330b1b7a727a94a9194e46d
Ben Eggers <beggerss@cs.washington.edu>
2014-07-27 13:27:29 -0700

    little more progress

--- interpreter.hs
+++ interpreter.hs
@@ -3,16 +3,17 @@
 
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 data LispVal = Atom String
-                | List [LispVal]
-                | DottedList [LispVal] LispVal
-                | Number Integer
-                | String String
-                | Bool Bool
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
 
 parseString :: Parser LispVal
 parseString = do
@@ -21,8 +22,26 @@ parseString = do
     char '"'
     return $ String x
 
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let atom = first:rest
+    return $ case atom of
+        "#t" -> Bool True
+        "#f" -> Bool False
+        _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
     Left err -> "Error: " ++ show err
     Right _ -> "Found value"
 
